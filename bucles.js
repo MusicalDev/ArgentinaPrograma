150e1c57e7a1e637ec80628312dbc24ca9ac47a1
Uli <135558341+MusicalDev@users.noreply.github.com>
2023-06-04 20:27:56 -0300

    primer commit del ArgentinaPrograma

---
+++ bucles.js
@@ -0,0 +1,40 @@
+// {for (let i = 0; i < 10; i++) {
+//     console.log(i); 
+// }
+// let contador = 0;
+
+// while (contador < 10) {
+//     console.log(contador);
+//     contador++;   
+// }
+// }
+
+// let contador = 0;
+// do {console.log(contador);
+//     contador++;
+// } while (contador < 10);
+
+// let miLista = [0,1,2,3,4,5,6,7,8,9];
+// for (let elemento of miLista) {
+//     console.log(elemento);  
+// }
+// const persona = {
+//     nombre: " Ulises",
+//     edad: " 34",
+//     profesión: " Músico",
+// };
+// for (let key in persona) {
+//     console.log(key + ":" + persona[key]);
+// }
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+//   if (i === 5) {
+//     break; 
+//   }
+// }
+// for (let i = 0; i < 10; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
